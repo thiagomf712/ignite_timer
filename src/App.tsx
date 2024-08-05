23f184f635e156c96999679c3da172bd470c9166
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-05 10:06:01 -0300

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyles } from './styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
+      <h1>Hello</h1>
+    </ThemeProvider>
+  )
+}
